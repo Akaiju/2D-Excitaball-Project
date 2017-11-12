273d45a1ff41cb155c977dccb3a67533a4be724d
Akaiju <john.buchaniec@loop.colum.edu>
2017-11-12 17:14:34 -0600

    Finished project, added restarts and audio to title

--- Assets/Scripts/DemoLevelCoin.cs
+++ Assets/Scripts/DemoLevelCoin.cs
@@ -20,19 +20,15 @@ public class DemoLevelCoin : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    public static int CoinCount;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Debug.Log("Coin Touched");
 
         if(collision.gameObject.tag == "Player")
         {
             audioSource.Play();
             boxCollider2D.enabled = false;
             spriteRenderer.enabled = false;
-            CoinCount++;
-            Debug.Log("Coin Count " + CoinCount);
+    
             Destroy(gameObject, audioSource.clip.length + 0.2f);
 
             SceneManager.LoadScene(leveltoLoad);
